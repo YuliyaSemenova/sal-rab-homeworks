84ef3125dd096f35ea36bfb73ce6cf15bdc02294
Yuliya Semenova <ybasanec@yandex.ru>
2022-03-12 23:33:11 +0300

    done 2and3

--- task_1/solution.js
+++ task_1/solution.js
@@ -27,6 +27,8 @@ function addInCartCountChange(value) {
 
     // Задание №1.2. Увеличение количества в корзине
     let oldValue = value;
+    let newValue;
+    newValue = oldValue + 1;
 
     // создайте переменную newValue
     // присвойте ей значение oldValue
@@ -42,6 +44,12 @@ function addInCartSumChange(sum, delta) {
     let difference = delta;
 
     // Задание №1.3. Увеличение суммы в корзине
+let newSum;
+newSum = oldSum + difference;
+let newSumText;
+newSumText = newSum + " ₽";
+newSumText = `${newSum} ₽`;
+
 
     // создайте переменную newSum и присвойте ей сумму oldSum и difference
     // создайте переменную newSumText и присвойте ей результат склеивания переменной newSum и текста ' ₽'
