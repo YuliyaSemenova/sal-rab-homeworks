8b52f94a85d5816347878c7542bed4fb61581066
Yuliya Semenova <ybasanec@yandex.ru>
2022-03-22 23:40:48 +0300

    done

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,20 +34,18 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
-
+    let data = {client, goods: [], order: {address, sum}};
     let countOfGoods = goods.length;
 
     for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
+    let good = {title: goods[i].title, count:goods[i].count}       
+    data.goods.push(good);
     }
 
     data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
-
-    data.client = 'Иван';
-
+    data.order.sum = sum;
+    data.client = name + phone;
     let jsonData = JSON.stringify(data);
-
+    
     return jsonData;
 }
